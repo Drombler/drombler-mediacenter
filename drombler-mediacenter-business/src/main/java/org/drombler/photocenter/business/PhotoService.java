ec2001a0163bca796038b83d6373232fb7056d2a
Florian Brunner <fbrunnerreg@gmx.ch>
2020-11-21 10:13:02 +0100

    photocenter -> mediacenter

---
+++ drombler-mediacenter-business/src/main/java/org/drombler/photocenter/business/PhotoService.java
@@ -0,0 +1,13 @@
+package org.drombler.photocenter.business;
+
+
+/**
+ * @author Florian
+ */
+
+public interface PhotoService {
+
+//    List<Event> getEventsByOwnersContaining(DromblerId owner);
+//
+//    Event createEvent(DromblerUserId dromblerUserId, Event event);
+}
---
+++ drombler-mediacenter-web/src/test/java/org/drombler/photocenter/web/PhotoCenterAppTests.java
@@ -0,0 +1,15 @@
+package org.drombler.photocenter.web;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+public class PhotoCenterAppTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
